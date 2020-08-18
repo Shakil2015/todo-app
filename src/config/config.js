81b213b3151f8f3500f662aa0927bfeeab3a56fe
Shakil2015 <sshakil496@gmail.com>
2020-08-18 17:40:48 +0600

    firebase-setup

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgT-8OkRRpttoUH1iJ22IFhB7crgqJ6S0",
+    authDomain: "todo-app-64cc1.firebaseapp.com",
+    databaseURL: "https://todo-app-64cc1.firebaseio.com",
+    projectId: "todo-app-64cc1",
+    storageBucket: "todo-app-64cc1.appspot.com",
+    messagingSenderId: "787794578824",
+    appId: "1:787794578824:web:0d87b44437c9285bec3e3d",
+    measurementId: "G-BCPS1VNQSP"
+  };
