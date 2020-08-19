cb0056094c45906c3682694cb58565bd3e83036c
Shakil2015 <sshakil496@gmail.com>
2020-08-19 17:26:21 +0600

    office -19/08/2020

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
+import './css/App.css'
 
 function App() {
+  const[todo,setTodo] =useState(['Tack the pen first.','Take the book second'])
+  const [input,setInput]=useState('')
+  console.log(input)
+  const handleChange = (e)=>{
+       e.preventDefault()
+   
+         console.log('is clicked')
+         setTodo([...todo,input])
+         setInput('')
+  }
   return (
     <div className="App">
-     <p>Shakil</p>
+     <p className='app_title'>Shakil's Daily Worklist</p>
+      <form>
+        <input value={input} onChange={e=>setInput(e.target.value)} />
+        <button type='submit' onClick={handleChange}>add todo</button>
+      </form>
+     
+     <ul>{
+       todo.map(item=>(
+        <li key={Math.random()}>{item}</li>
+       ))
+       }
+        
+     </ul>
     </div>
   );
 }
